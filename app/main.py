5b2fa2ce78d8c1e269180eb4b34ac24b8b26e746
Graylee0128 <125956961+Graylee0128@users.noreply.github.com>
2026-07-24 02:17:32 +0000

    feat:upgrade to v8

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="0717 CI/CD Demo")
 
 @app.get("/")
 def root():
-    return {"service": "0717-cicd-demo", "version": VERSION, "build": "v7"}
+    return {"service": "0717-cicd-demo", "version": VERSION, "build": "v8"}
 
 
 @app.get("/health")
